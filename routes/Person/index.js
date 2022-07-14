e3003073eaa91e229db821b559553636864610cd
Elcio R <elcioroberto1995@gmail.com>
2022-07-14 12:57:19 -0300

    feat: criando rota get '/person'

--- routes/Person/index.js
+++ routes/Person/index.js
@@ -16,4 +16,18 @@ router.post('/person', validateDataRequestPerson, async (request, response) => {
         return response.status(400).json({ 'error': error });
     }
 });
+
+router.get('/person', async (request, response) => {
+    try {
+
+        const person = await Person.find();
+
+        return response.status(200).json({ 'data': person });
+    }
+    catch (error) {
+        response.status(400).json({ 'error': error });
+    }
+});
+
+
 module.exports = router;
