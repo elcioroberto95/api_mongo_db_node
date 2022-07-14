d6adaab505a29d2bf83a08599f76c0134a75d2fe
Elcio R <elcioroberto1995@gmail.com>
2022-07-14 12:55:02 -0300

    fix: removend console.log não utilizado

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(urlMongoDb)
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log(`Server running on port: ${process.env.PORT}`);
-            console.lo
+        
         })
     }).
     catch(error => {
