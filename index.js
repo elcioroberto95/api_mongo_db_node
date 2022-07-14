97a2221e9e2dc376eca18c7449fc43544814796f
Elcio R <elcioroberto1995@gmail.com>
2022-07-14 12:54:34 -0300

    refactor: criando variavel com url do mongodb

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+require('dotenv').config();
 const express = require('express');
-const { port } = require('./env_variables');
-
-
+const mongoose = require('mongoose');
+const routes = require('./routes/Person')
 const app = express();
+const urlMongoDb = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@apicluster.tfhke.mongodb.net/bancodaapi?retryWrites=true&w=majority"
+
 app.use(express.urlencoded({
     extended: true
 }))
 app.use(express.json());
+app.use(routes)
 
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-})
+
+mongoose.connect(urlMongoDb)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port: ${process.env.PORT}`);
+            console.lo
+        })
+    }).
+    catch(error => {
+        console.log(error);
+    })
+
